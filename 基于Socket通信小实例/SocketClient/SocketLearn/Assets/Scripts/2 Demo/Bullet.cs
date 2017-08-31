d2ffb53f44f3e9262622d7eb8b7e073b763413e3
oxfords561 <18924192170@189.cn>
2017-08-31 19:38:01 +0800

    添加测试场景
    测试子弹发射功能以及之后的功能预备场景

--- 基于Socket通信小实例/SocketClient/SocketLearn/Assets/Scripts/2 Demo/Bullet.cs
+++ 基于Socket通信小实例/SocketClient/SocketLearn/Assets/Scripts/2 Demo/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        Debug.Log(transform.localPosition);
         Destroy(gameObject,5f);
 	}
 	
---
+++ 基于Socket通信小实例/SocketClient/SocketLearn/Assets/Scripts/Test/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public GameObject bullet;
+    private Transform bulletPos;
+
+	// Use this for initialization
+	void Start () {
+        bulletPos = transform.Find("BulletPos");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject go = Instantiate<GameObject>(bullet, bulletPos.position,Quaternion.identity);
+            go.GetComponent<Rigidbody>().AddForce(bulletPos.forward*8000);
+        }
+	}
+}
