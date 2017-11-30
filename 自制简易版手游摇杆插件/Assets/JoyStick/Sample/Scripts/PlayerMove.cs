aab66dbfb0dc884ce292ef497be6b122973da1e5
oxfords561 <18924192170@189.cn>
2017-11-30 12:13:33 +0800

    自制简易版手游摇杆插件
    自定义UGUI 做成的插件

---
+++ 自制简易版手游摇杆插件/Assets/JoyStick/Sample/Scripts/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private SticktController sticktController;
+    private Rigidbody mRigibody;
+    private float moveSpeed = 3f;
+    private float offsetX;
+    private float offsetY;
+
+	void Start () {
+        mRigibody = GetComponent<Rigidbody>();
+        sticktController = GameObject.Find("Canvas").transform.Find("JoyStick").GetComponent<SticktController>();
+        if(sticktController != null)
+            sticktController.OnMoveStickt += Move;
+    }
+	
+    void Move(float offsetX, float offsetY)
+    {
+        if (offsetY != 0 || offsetX != 0)
+        {
+            Quaternion newDirection = Quaternion.LookRotation(new Vector3(offsetX, 0, offsetY), Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, newDirection, Time.deltaTime * 10f);
+
+            mRigibody.MovePosition(transform.localPosition + (Vector3.forward * offsetY + (Vector3.right * offsetX)).normalized * moveSpeed * Time.deltaTime);
+
+            
+        }
+    }
+   
+}
---
+++ 自制简易版手游摇杆插件/Assets/JoyStick/Scripts/SticktController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SticktController : MonoBehaviour {
+
+	private RectTransform sticktTransform;
+	private RectTransform sticktBgTransform;
+	//private GameObject touchRotatePanel;
+	private Vector3 firstSticktPos;
+	private Vector2 firstSticktPos2;
+	private bool isUp = false;
+	private bool isDown = false;
+	private float backSpeed = 4f;
+	private float radius;
+	private float offsetX;
+	private float offsetY;
+	private float rotateX;
+	private float rotateY;
+	private Vector3 downMousePos;
+
+	public Action<float,float> OnMoveStickt;
+	public Action<float,float> OnTouchRotate;
+	public Action OnJumpClick;
+	public Action OnShootClick;
+
+	void Start () {
+		//touchRotatePanel = transform.Find ("TouchRotate").gameObject;
+		sticktTransform = transform.Find ("SticktBg/Stickt").GetComponent<RectTransform> ();
+		sticktBgTransform = transform.Find ("SticktBg").GetComponent<RectTransform> ();
+		firstSticktPos = sticktTransform.position;
+		firstSticktPos2 = sticktTransform.anchoredPosition;
+		radius =  sticktBgTransform.sizeDelta.x / 2;
+
+	}
+	
+	void Update () {
+
+		if (isUp) {
+			sticktTransform.position = Vector3.Lerp (sticktTransform.position,firstSticktPos,Time.deltaTime * backSpeed);
+		}
+
+		if (isDown) {
+			if (OnMoveStickt != null) {
+				OnMoveStickt (offsetX, offsetY);
+			}
+		}
+	}
+
+	public void OnSticktDrag(){
+		
+		float distance = Vector3.Distance (sticktBgTransform.position, Input.mousePosition);
+		if (distance < radius) {
+			sticktTransform.position = Input.mousePosition;
+
+		} else {
+			Vector3 targetDirection = Input.mousePosition - firstSticktPos;
+			sticktTransform.position = firstSticktPos +  targetDirection.normalized * radius;
+			 
+		}
+
+		offsetX = sticktTransform.position.x - firstSticktPos.x;
+		offsetY = sticktTransform.position.y - firstSticktPos.y;
+	}
+
+	public void OnPointerUp(){
+		isUp = true;
+		isDown = false;
+	}
+
+	public void OnPointerDown(){
+		isUp = false;
+		isDown = true;
+	}
+
+	//public void OnTouchDragRotate(){
+
+
+	//	rotateX = Input.mousePosition.x - downMousePos.x;
+	//	rotateY = Input.mousePosition.y - downMousePos.y;
+
+	//	if (OnTouchRotate != null) {
+	//		OnTouchRotate (rotateX, rotateY);
+	//	}
+	//}
+
+	public void OnTouchDragDown(){
+		downMousePos = Input.mousePosition;
+	}
+
+	public void OnJumpPointerDown(){
+		if (OnJumpClick != null) {
+			OnJumpClick ();
+		}
+	}
+
+	public void OnShootPointerDown(){
+		if (OnShootClick != null) {
+			OnShootClick ();
+		}
+	}
+
+	public void OnQuitGame(){
+		Application.Quit ();
+	}
+}
